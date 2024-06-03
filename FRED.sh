cd7a9fdae04a449912f6509af3b2418911c76aca
Chenxi Liu <46647878+ChenxiLiu-HNU@users.noreply.github.com>
2024-06-03 10:47:27 +0800

    Update FRED.sh

--- FRED.sh
+++ FRED.sh
@@ -30,7 +30,7 @@ for seq_len in "${seq_lens[@]}"; do
                   --num_nodes 107 \
                   --seq_len $seq_len \
                   --pred_len $pred_len \
-                  --epochs 100 \
+                  --epochs 50 \
                   --seed 8888 \
                   --channel $channel \
                   --head 8 \
