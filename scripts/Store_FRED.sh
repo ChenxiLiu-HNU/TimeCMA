d1ee6296bea58ff0de1b9dd0f58402edf7a100a6
Chenxi Liu <46647878+ChenxiLiu-HNU@users.noreply.github.com>
2024-06-04 14:46:13 +0800

    Update Store_FRED.sh

--- scripts/Store_FRED.sh
+++ scripts/Store_FRED.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PYTHONPATH=/path/to/project_root:$PYTHONPATH
 
 nohup python storage/store_emb.py --divide train --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_train_p5.log &
 nohup python storage/store_emb.py --divide val --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_val_p5.log &
