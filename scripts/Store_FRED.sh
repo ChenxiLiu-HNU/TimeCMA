19fa866c2c2e7121aa4ce6e78d117ac6378efe54
Chenxi Liu <46647878+ChenxiLiu-HNU@users.noreply.github.com>
2024-06-04 14:45:55 +0800

    Update Store_FRED.sh

--- scripts/Store_FRED.sh
+++ scripts/Store_FRED.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup python store_emb.py --divide train --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_train_p5.log &
-nohup python store_emb.py --divide val --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_val_p5.log &
-nohup python store_emb.py --divide test --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_test_p5.log &
+nohup python storage/store_emb.py --divide train --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_train_p5.log &
+nohup python storage/store_emb.py --divide val --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_val_p5.log &
+nohup python storage/store_emb.py --divide test --data_path FRED --device cuda:7 --num_nodes 107 --input_len 36 --output_len 24 > ./Results/emb_time/FRED_test_p5.log &
